8a58ad370575b217da696d321c0402f1c4d1894c
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-11-16 06:37:25 +0700

    feat(crud) : add rejected function in situs

--- routes/situs.js
+++ routes/situs.js
@@ -149,6 +149,29 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res, next)
     }
 });
 
+router.put('/approve/:id', authenticateToken, isVerifier, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query(
+            `
+            UPDATE objek_temuan
+            SET status_verifikasi = 'rejected'
+            WHERE objek_id = $1 RETURNING *
+            `
+            [id]
+        );
+        
+        if (result.rows.length === 0){
+            return res.status(404).json({message: 'Objek tidak ditemukan!'});
+        }
+
+        res.json({message: 'Objek berhasil direject', data: result.rows[0]});
+    } catch (error) {
+        console.error('Error reject objek', error);
+        res.status(500).json({message: 'Error di server'});
+    }
+});
+
 router.delete('/:id', authenticateToken, isAdmin, async (req, res) => {
     try {
         const {id} = req.params;
