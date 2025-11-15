afb1455e9efca62ff48bbc9033f1bff5d3a85e25
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-11-16 06:59:56 +0700

    feat(crud) : add rejected function in situs

--- routes/situs.js
+++ routes/situs.js
@@ -135,7 +135,7 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res, next)
         );
 
         if (updateSitus.rows.length === 0){
-            return res.status(404).json({ message : 'SItus tak ditemukan'});
+            return res.status(404).json({ message : 'Situs tak ditemukan'});
         }
 
         res.json({
@@ -149,26 +149,27 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res, next)
     }
 });
 
-router.put('/approve/:id', authenticateToken, isVerifier, async (req, res) => {
+router.put('/reject/:id', authenticateToken, isVerifier, async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const result = await pool.query(
-            `
-            UPDATE objek_temuan
-            SET status_verifikasi = 'rejected'
-            WHERE objek_id = $1 RETURNING *
-            `
+        
+        const {id} = req.params;
+        const updateSitus = await pool.query(
+            `UPDATE situs_arkeologi SET status_verifikasi = 'rejected' WHERE situs_id = $1 RETURNING *`,
             [id]
         );
-        
-        if (result.rows.length === 0){
-            return res.status(404).json({message: 'Objek tidak ditemukan!'});
+
+        if (updateSitus.rows.length === 0){
+            return res.status(404).json({ message : 'Situs tak ditemukan'});
         }
 
-        res.json({message: 'Objek berhasil direject', data: result.rows[0]});
+        res.json({
+            message: 'Situs berhasil direject!',
+            data: updateSitus.rows[0]
+        });
+
     } catch (error) {
-        console.error('Error reject objek', error);
-        res.status(500).json({message: 'Error di server'});
+        console.error('Error saat reject situs:', error);
+        res.status(500).json({message: 'Terjadi error di server!'});
     }
 });
 
