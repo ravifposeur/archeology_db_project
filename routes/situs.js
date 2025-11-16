2fea687bd3ff41232e4a986219f4fc9c3c3ced5a
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-11-16 08:21:08 +0700

    feat(crud) : reject function in situs.js

--- routes/situs.js
+++ routes/situs.js
@@ -149,6 +149,29 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res, next)
     }
 });
 
+router.put('/reject/:id', authenticateToken, isVerifier, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query(
+            `
+            UPDATE situs_arkeologi
+            SET status_verifikasi = 'rejected'
+            WHERE situs_id = $1 RETURNING *
+            `
+            [id]
+        );
+        
+        if (result.rows.length === 0){
+            return res.status(404).json({message: 'Situs tidak ditemukan!'});
+        }
+
+        res.json({message: 'Situs berhasil direject', data: result.rows[0]});
+    } catch (error) {
+        console.error('Error reject Situs', error);
+        res.status(500).json({message: 'Error di server'});
+    }
+});
+
 router.delete('/:id', authenticateToken, isAdmin, async (req, res) => {
     try {
         const {id} = req.params;
