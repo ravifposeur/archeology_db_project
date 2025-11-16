97722468e3141468f12902697cd4bd22a894a94d
ravifposeur <ravifgayuhwicaksono@gmail.com>
2025-11-16 07:37:01 +0700

    feat(crud) : add delete crud in objek_temuan.js

--- routes/objek_temuan.js
+++ routes/objek_temuan.js
@@ -110,7 +110,7 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res) => {
 });
 
 // REJECT OBJECT
-router.put('/approve/:id', authenticateToken, isVerifier, async (req, res) => {
+router.put('/reject/:id', authenticateToken, isVerifier, async (req, res) => {
     try {
         const { id } = req.params;
         const result = await pool.query(
@@ -130,6 +130,35 @@ router.put('/approve/:id', authenticateToken, isVerifier, async (req, res) => {
     } catch (error) {
         console.error('Error reject objek', error);
         res.status(500).json({message: 'Error di server'});
+    }
+});
+
+router.delete('/:id', authenticateToken, isAdmin, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await pool.query(
+            "DELETE FROM objek_temuan WHERE objek_id = $1 RETURNING *",
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({message: 'Objek tidak ditemukan'});
+        }
+
+        res.json({
+            message: 'Objek berhasil dihapus',
+            data: result.rows[0]
+        });
+    } catch (error) {
+        console.error('Error saat hapus Objek Temuan', error);
+
+        if(error.code == '23503') {
+            return res.status(400).json({
+                message: 'Situs ini masih punya tabel yang terhubung'
+            });
+        }
+
+        res.status(500).json({message: 'Error di server.'});
     }
 });
 
